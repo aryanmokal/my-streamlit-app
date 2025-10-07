11294680199b7331505e0b1aae40c27daa3d3c72
aryanmokal <aryanmokal11@gmail.com>
2025-10-07 19:07:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.title("💕Hi shreyu my Love💕")
+st.markdown(
+    """ 
+    🥰🥰🥰a short website gift to you from your pyara bacchu🥰🥰 
+🥰
+    
+    🤗🤗hope my babe likes it🤗🤗
+    
+    
+    """)
+if st.button("❤Send love to me!❤"):
+   st.write("💝"*66)
+   st.write("thanks for the love babe love you tooooo")
+if st.button("❤next❤"):    
+   st.write("............. from the first day i saw u in dada class🥹.............") 
+if st.button("❤agge batao❤"):
+   st.write ("......to becoming classmates📚📚📚....... ")
+if st.button("❤next kya hua ❤"):
+   st.write ("fir hum electronics batchmate ban gaye ") 
+if st.button("❤fir kya hua❤"):
+   st.write("hum chess khela practical kiye sath me ")
+if st.button("❤fir kya hua babe❤"):
+   st.write("hum friends bane")
+if st.button("❤uske bad kya hua❤"):
+   st.write ("hum best friends ban gaye ")
+if st.button("❤last me kya hua❤"):
+   st.write("after almost 3 years of your presense in my life you finally become mine and No matter what life brings, I’ll always be right here — holding your hand, protecting your peace,supporting you even if the whole world gets against you i will be your only supporter and standing by your side. Forever. its a promise from my heart , always with you my shreyu my world  . 💞🫶 ")   
+if st.button("😊😊if you want to see Aaru ka female version click here😊😊"):
+   st.write("here it is ")      
+   st.image("https://ik.imagekit.io/xa3har9bq/Screenshot%202025-10-06%20222834.png?updatedAt=1759770271151", caption=("❤❤❤❤❤❤❤My awesome baby girl ❤❤❤❤❤❤❤!"))
+
+st.camera_input("ek smile karke photo khejde. your smile is everything to me 🥹🥹")
+st.title ("feeling low ?")
+if st.button("🩵click her to call and share everything 🩵"):
+   st.write("8668282351")
+   st.write("💗your pyaara sa bacchu is always available for you 💗")
+st.title(" always remember one thing ")
+st.write(" i am there with you your home your i will give my best to be responsible , caring ,supportive and helpful everytime as you have given me this beutiful responsiblity of being your 2 nd baba i will take care of you forever  ")
+st.title ("💗from your pyarasa bacchu with love 💗")
